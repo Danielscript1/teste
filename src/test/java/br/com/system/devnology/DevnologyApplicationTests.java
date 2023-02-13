99f71e464914b21d4c1c70471fc9fe515a6c2b35
Danielscript1 <danielsousagit@gmail.com>
2023-02-13 18:43:03 -0300

    add

--- src/test/java/br/com/system/devnology/DevnologyApplicationTests.java
+++ src/test/java/br/com/system/devnology/DevnologyApplicationTests.java
@@ -19,61 +19,6 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 class DevnologyApplicationTests {
 
-	@Autowired
-	ResourceLinks resourceLinks;
 
-	@Test
-	@Order(1)
-	public void shouldSavedLinkBancoTest() {
-
-		Links savedLinks = resourceLinks.insert(new Links(1l,"urlteste.com","titulo")).getBody();
-		Assertions.assertThat(savedLinks.getId()).isGreaterThan(0);
-
-	}
-
-	@Test
-	@Order(2)
-	public void shouldSearchLinkIdTest(){
-
-		Links savedLinks = resourceLinks.insert(new Links(1l,"urlteste.com","titulo")).getBody();
-		Links links = resourceLinks.findId(1L).getBody();
-		Assertions.assertThat(links.getId()).isEqualTo(1L);
-
-	}
-
-	@Test
-	@Order(3)
-	@Rollback(value = false)
-	public void shouldUpdateLinkTest(){
-
-		Links savedLinks = resourceLinks.insert(new Links(1l,"urlteste.com","titulo")).getBody();
-		Links links = resourceLinks.findId(1L).getBody();
-		links.setTitle("novo titulo");
-		Links link = resourceLinks.insert(links).getBody();
-		Assertions.assertThat(link.getTitle()).isEqualTo("novo titulo");
-
-	}
-
-	@Test
-	@Order(4)
-	public void shouldSearchPageLinkAllTest(){
-		Links savedLinks = resourceLinks.insert(new Links(1l,"urlteste.com","titulo")).getBody();
-		ResponseEntity<Page<Links>> links = resourceLinks.findPage(0, 24, "title", String.valueOf(Sort.Direction.ASC));
-		Assertions.assertThat(links.getBody().getSize()).isEqualTo(24);
-		Assertions.assertThat(links.getBody().hasContent()).isEqualTo(links.getBody().getSize()>0);
-
-	}
-
-	@Test
-	@Order(5)
-	@Rollback(false)
-	public void shouldDeleteProductTest() {
-
-		Links link = resourceLinks.insert(new Links(1l,"urlteste.com","titulo")).getBody();
-		Links linkDelete = resourceLinks.findId(1l).getBody();
-		Links delete = resourceLinks.delete(linkDelete.getId()).getBody();
-		Assertions.assertThat(delete).isNull();
-
-	}
 
 }
